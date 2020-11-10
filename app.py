936696eea586519ef414d72cdddddab361b617a1
purbu-engkh <epurev@khabuko.com>
2020-11-10 11:29:17 +0800

    added RRnet/residencereport

--- app.py
+++ app.py
@@ -37,6 +37,14 @@ def RRnet_privacy():
 def RRnet_accessibility():
     return render_template('/RRnet/accessibility.html')
 
+@app.route('/RRnet/residencereport/login')
+def RRnet_res_login():
+    return render_template('/RRnet/residencereport/login.html')
+
+@app.route('/RRnet/residencereport/password/reset')
+def RRnet_pass_reset():
+    return render_template('/RRnet/residencereport/password/reset.html')
+
 @app.route('/tabireg')
 def tabireg():
     return render_template('/tabireg/index.html')
